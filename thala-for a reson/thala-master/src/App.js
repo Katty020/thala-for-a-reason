47fc9e0ea5226b6afea03600dfd3c2fd7de0f31e
Aryan Katiyar <131390724+Katty020@users.noreply.github.com>
2024-01-15 17:34:30 +0530

    Update App.js

--- thala-for a reson/thala-master/src/App.js
+++ thala-for a reson/thala-master/src/App.js
@@ -67,7 +67,7 @@ function App() {
       <div className=" z-[20] fixed h-full flex flex-col gap-20 justify-center items-center">
         <div>
           <label className="text-white text-7xl">
-            Thala for a reason ?
+          KYA AAPKE ANDER BHI THALA HAI??
           </label>
         </div>
         <div>
@@ -80,21 +80,24 @@ function App() {
           ></input>
           <button
             onClick={isThala}
-            className="bg-yellow-500 h-[100px] w-auto p-5 text-5xl rounded-full"
+            className="bg-blue-500 h-[100px] w-auto p-5 text-5xl rounded-full border-yellow-400"
           >
-            Check
+            Check here
           </button>
         </div>
         <label className="text-white text-7xl ">
           {isthala === true ? (
-            <h1>Thala for a reason</h1>
+            <h1>KYA AAPKE ANDER BHI THALA HAI??</h1>
           ) : (
-            <h1>Naughty ho rha hai keyyyy</h1>
+            <h1>Check kar ke dekh Lo naaaaa</h1>
           )}
         </label>
         <label className="text-white text-3xl">
-          <h1>try writing not a 7 word</h1>
-          {/* <h1>try writing gambhir</h1> */}
+          <h1>7 to mat hi likhna bhai..
+          </h1>
+          <h1></h1>
+          <h3>Made for fun-*Katiyar*</h3>
+          <h3>🦁🦁🦁🦁🦁🦁🦁</h3>
         </label>
       </div>
     </div>
